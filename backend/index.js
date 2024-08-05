003eee8b8ffac1d7351e1d3e7b7fbb33d3ff218c
Martin <ilhammartin2@gmail.com>
2024-08-05 13:18:53 +0700

    install vercel

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,16 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
+
 app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
+app.use(express.json());
 app.use(uploadRoutes)
+
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`);
